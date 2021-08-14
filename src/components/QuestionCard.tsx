6cbcb40e8ef9651ef82dcdfd33d0ddc663e290a2
Consecratio <jcasique@gmail.com>
2021-08-13 17:13:37 -0700

    finished styling

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
 
+// Styles
+import { Wrapper, ButtonWrapper } from './QuestionCard.styles'
+
 // Types
 import { AnswerObject } from '../App'
 
@@ -22,21 +25,25 @@ const QuestionCard: React.FC<Props> = ({
 }) => {
 
     return (
-        <div>
+        <Wrapper>
             <p className="number">
                 Question: {questionNr} / {totalQuestions}
             </p>
             <p dangerouslySetInnerHTML={{ __html: question }} />
             <div>
                 {answers.map(answer => (
-                    <div key={answer}>
+                    <ButtonWrapper
+                        key={answer}
+                        correct={userAnswer?.correctAnswer === answer}
+                        userClicked={userAnswer?.answer === answer}
+                    >
                         <button disabled={userAnswer ? true : false} value={answer} onClick={callback}>
                             <span dangerouslySetInnerHTML={{ __html: answer }} />
                         </button>
-                    </div>
+                    </ButtonWrapper>
                 ))}
             </div>
-        </div>
+        </Wrapper>
     )
 }
 
